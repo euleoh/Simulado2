c68a7ed1f72c7bf3a39b63ef13ba12b8aa609d85
euleoh <shaolinmtadordeporcotyui@gmail.com>
2026-05-07 07:44:36 -0300

    update 07-05

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -4,15 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Produto extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
-    protected $fillable =[
-        'nome',
-        'valor',
-        'qtd_estoque',
-        'qtd_minima'
-    ];
+  protected $fillable = [
+    'nome',
+    'valor',
+    'qtd_estoque',
+    'qtd_minima'
+  ];
+
+  public function movimentacoes(): HasMany
+  {
+    return $this->hasMany(Movimentacao::class);
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,20 @@
 <?php
 
+use App\Livewire\Movimentacao\MovimentacaoCreate;
+use App\Livewire\Movimentacao\MovimentacaoIndex;
 use App\Livewire\Produto\ProdutoCreate;
 use App\Livewire\Produto\ProdutoEdit;
 use App\Livewire\Produto\ProdutoIndex;
+use App\Models\Produto;
 use Illuminate\Support\Facades\Route;
-
 
 Route::get('produto/create', ProdutoCreate::class)->name('produto.create');
 
+Route::get('produto', ProdutoIndex::class)->name('produto.index');
+
 Route::get('produto/edit/{id}', ProdutoEdit::class)->name('produto.edit');
 
-Route::get('produto/index', ProdutoIndex::class)->name('produto.index');
+Route::get('movimentacao/create', MovimentacaoCreate::class)->name('movimentacao.create');
+
+Route::get('movimentacao', MovimentacaoIndex::class)->name('movimentacao.index');
+
